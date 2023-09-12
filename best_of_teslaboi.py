b753bc25f69ca6bd6c0015fd1b9ed55529f77f96
Teslaboi3000 <142306632+Teslaboi3000@users.noreply.github.com>
2023-09-12 21:44:37 +0200

    change to program chooser

--- best_of_teslaboi.py
+++ best_of_teslaboi.py
@@ -3,36 +3,22 @@ from magic_text import magic_text
 from morse_translator import morse_translator
 from writer import writer
 
-i = 5
-while i < 10:
+while True:
     print('''|The Best Of Teslaboi!|
             All of Tesla's cool programs in one!
         ''')
     print('''There are 4 programs to choose from:
-            Choice Helper
-            Magic Text
-            Morse Translator
-            Writer
-          Or you can write stop to close the program!
+            1) Choice Helper
+            2) Magic Text
+            3) Morse Translator
+            4) Writer
         ''')
-    program_choice = input('Write the name of the feature you want to use: ').lower()
-    
-    if  program_choice == 'choice helper':
+    program_choice = input('Write the number of the program you want to use in this list: ').lower()
+    if program_choice == '1':
         choice_helper()
-        print('''
-        ''')
-    elif program_choice == 'magic text':
+    elif program_choice == '2':
         magic_text()
-        print('''
-        ''')
-    elif program_choice == 'morse translator':
+    elif program_choice == '3':
         morse_translator()
-        print('''
-        ''')
-    elif program_choice == 'writer':
+    elif program_choice == '4':
         writer()
-        print('''
-        ''')
-    else:
-        print('''I still haven't made that program...just yet!
-              ''')
